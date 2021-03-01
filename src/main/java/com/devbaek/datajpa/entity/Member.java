42d15c62ce31c499e063281fc6442d9a042fbcba
bbaing4564 <bbaing4564@gmail.com>
2021-03-01 22:25:44 +0900

    apply spring-jpa named query

--- src/main/java/com/devbaek/datajpa/entity/Member.java
+++ src/main/java/com/devbaek/datajpa/entity/Member.java
@@ -10,6 +10,9 @@ import java.util.Optional;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NamedQuery(name="Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
--- src/main/java/com/devbaek/datajpa/repository/MemberRepository.java
+++ src/main/java/com/devbaek/datajpa/repository/MemberRepository.java
@@ -2,11 +2,15 @@ package com.devbaek.datajpa.repository;
 
 import com.devbaek.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByUsername(String username);
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+//    @Query(name = "Member.findByUsername") // 이름이 같을 때에 생략 가능
+    List<Member> findByUsername(@Param("username") String username);
 }
